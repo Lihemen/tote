6272780f1c07f9104564cd832223af6eb409f05c
Hemense Lan <lihemen@gmail.com>
2022-06-13 01:32:17 +0100

    cart, fixed overflow issue

--- script.js
+++ script.js
@@ -23,18 +23,16 @@ $(".products_grid").slick({
   speed: 1000,
   slidesToShow: 3,
   slidesToScroll: 1,
-  adaptiveHeight: true,
+  // adaptiveHeight: true,
   autoplay: true,
   prevArrow: $(".prev"),
   nextArrow: $(".next"),
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 500,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 1,
         slidesToScroll: 1,
-        infinite: true,
-        dots: true,
       },
     },
     {
@@ -45,9 +43,9 @@ $(".products_grid").slick({
       },
     },
     {
-      breakpoint: 500,
+      breakpoint: 1024,
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 3,
         slidesToScroll: 1,
       },
     },
@@ -58,7 +56,7 @@ $(".reviews_grid").slick({
   dots: false,
   infinite: false,
   speed: 1000,
-  slidesToShow: 1,
+  slidesToShow: 2,
   slidesToScroll: 1,
   prevArrow: $("#reviewLeft"),
   nextArrow: $("#reviewRight"),
@@ -66,10 +64,76 @@ $(".reviews_grid").slick({
     {
       breakpoint: 800,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 1,
         slidesToScroll: 1,
-        adaptiveHeight: true,
       },
     },
   ],
 });
+
+const add_to_cart_btns = document.querySelectorAll(".add_to_cart");
+
+add_to_cart_btns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const product_id = event.target.dataset.product - id;
+  });
+});
+
+async function getAProduct(id) {
+  const response = await fetch(`products.json`);
+  const data = await response.json();
+  return data.filter((product) => product.id === id);
+}
+
+class Cart {
+  constructor() {
+    this.items = [];
+  }
+
+  addToCart(item) {
+    const in_cart = this.items.find((el) => el._id === item._id);
+
+    if (in_cart) {
+      return; // change to alert later
+    }
+
+    return this.items.push({ ...item, quantity: 1 });
+  }
+
+  removeFromCart(id) {
+    return this.items.filter((item) => item._id !== id);
+  }
+
+  get total() {
+    return this.items.reduce((obj, acc) => {
+      return acc + parseFloat(obj.price) * parseInt(obj.quantity);
+    });
+  }
+
+  increaseQty(id) {
+    return this.items.map((item) => {
+      if (item._id === id) {
+        return {
+          ...item,
+          quantity: quantity + 1,
+        };
+      }
+      return item;
+    });
+  }
+
+  decreaseQty(id) {
+    return this.items.map((item) => {
+      if (item._id === id && item.quantity > 1) {
+        return {
+          ...item,
+          quantity: quantity - 1,
+        };
+      }
+      return item;
+    });
+  }
+}
+
+// Cart Schema
